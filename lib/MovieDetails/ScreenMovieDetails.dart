47cb773d483db6f0f547085c495c50cb44206948
MR <mirsa@kali.kali>
2022-04-11 07:59:21 +0530

    Readmore button added

--- lib/MovieDetails/ScreenMovieDetails.dart
+++ lib/MovieDetails/ScreenMovieDetails.dart
@@ -1,67 +1,131 @@
 import 'package:apisample/HomePage/MyHomePage.dart';
 import 'package:apisample/MovieDetails/MovieDetailsModel.dart';
-import 'package:apisample/MovieDetails/fetchdetails.dart';
+import 'package:apisample/MovieDetails/fetchDetails.dart';
 import 'package:flutter/material.dart';
 
-class ScreenMovieDetails extends StatelessWidget {
-  const ScreenMovieDetails({Key? key, required this.id}) : super(key: key);
+class ScreenMovieDetails extends StatefulWidget {
+  const ScreenMovieDetails(
+      {Key? key, required this.id})
+      : super(key: key);
   final String id;
 
+  @override
+  State<ScreenMovieDetails> createState() =>
+      _ScreenMovieDetailsState();
+}
+
+class _ScreenMovieDetailsState
+    extends State<ScreenMovieDetails> {
   @override
   Widget build(BuildContext context) {
     bool isReadmore = false;
     return SafeArea(
       child: Scaffold(
         body: FutureBuilder(
-            future: fetchMovieDetails(id),
+            future: fetchMovieDetails(widget.id),
             builder: (BuildContext context,
-                AsyncSnapshot<MovieDetailsModel> snapshot) {
+                AsyncSnapshot<MovieDetailsModel>
+                    snapshot) {
               if (snapshot.hasData) {
                 return Container(
                   color: Colors.black,
-                  height: MediaQuery.of(context).size.height,
-                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context)
+                      .size
+                      .height,
+                  width: MediaQuery.of(context)
+                      .size
+                      .width,
                   child: ListView(
                     children: [
                       Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
+                        mainAxisAlignment:
+                            MainAxisAlignment
+                                .start,
                         children: [
                           const SizedBox(
                             height: 30,
                           ),
                           Container(
-                            height: MediaQuery.of(context).size.height / 2.5,
+                            height: MediaQuery.of(
+                                        context)
+                                    .size
+                                    .height /
+                                2.5,
                             decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10)),
+                                borderRadius:
+                                    BorderRadius
+                                        .circular(
+                                            10)),
                             child: Image.network(
-                              baseUrl + snapshot.data!.posterPath.toString(),
+                              baseUrl +
+                                  snapshot.data!
+                                      .posterPath
+                                      .toString(),
                               fit: BoxFit.fill,
                             ),
                           ),
-                          const SizedBox(height: 10),
+                          const SizedBox(
+                              height: 10),
                           Column(
                             children: [
                               Text(
-                                snapshot.data!.title.toString(),
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
+                                snapshot
+                                    .data!.title
+                                    .toString(),
+                                textAlign:
+                                    TextAlign
+                                        .center,
+                                style:
+                                    const TextStyle(
                                   fontSize: 26,
-                                  color: Colors.white,
+                                  color: Colors
+                                      .white,
                                 ),
                               ),
-
-                              Text(
-                                snapshot.data!.overview.toString(),
-                                style: TextStyle(
-                                  fontSize: 26,
-                                  color: Colors.white,
-                                ),
+                              SizedBox(
+                                height: 30,
                               ),
                               Text(
-                                "Average Vote" + snapshot.data!.voteAverage.toString(),
-                                style: TextStyle(
+                                snapshot.data!
+                                    .overview
+                                    .toString(),
+                                style:
+                                    const TextStyle(
+                                  fontSize: 20,
+                                  color: Colors
+                                      .white,
+                                ),
+                                maxLines:
+                                    isReadmore
+                                        ? null
+                                        : 1,
+                                overflow: isReadmore
+                                    ? TextOverflow
+                                        .visible
+                                    : TextOverflow
+                                        .ellipsis,
+                              ),
+                              TextButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      isReadmore =
+                                          !isReadmore;
+                                      print(isReadmore);
+                                    });
+                                  },
+                                  child: Text((isReadmore
+                                      ? "Read Less"
+                                      : "Read More"))),
+                              Text(
+                                "Average Vote" +
+                                    snapshot.data!
+                                        .voteAverage
+                                        .toString(),
+                                style:
+                                    const TextStyle(
                                   fontSize: 26,
-                                  color: Colors.white,
+                                  color: Colors
+                                      .white,
                                 ),
                               ),
                             ],
@@ -72,7 +136,9 @@ class ScreenMovieDetails extends StatelessWidget {
                   ),
                 );
               }
-              return Center(child: CircularProgressIndicator());
+              return const Center(
+                  child:
+                      CircularProgressIndicator());
             }),
       ),
     );
